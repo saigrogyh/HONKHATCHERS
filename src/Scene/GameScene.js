a1d59aaf1de6c17e51a12b948f10d6b980df4128
Tanapat Krowattanajaroenkit <141905443+TanapatZ@users.noreply.github.com>
2023-12-08 17:46:18 +0700

    Add files via upload

--- src/Scene/GameScene.js
+++ src/Scene/GameScene.js
@@ -23,28 +23,31 @@ class GameScene extends Phaser.Scene {
     this.shoot;
     this.spawntime = 0;
     this.breaktime = 0;
+    this.boarder;
+    this.button;
   }
 
   preload() {
+    // this.load.image("boarder","/assets/image/game-scene/components/jumppad2.png")
     this.load.image(
-      "bg-pink",
-      "assets/image/game-scene/background/background-pink.png"
+      "backgound",
+      "assets/image/game-scene/background/backgound_game.png"
     );
-    this.load.image("bullet", "assets/image/game-scene/components/box.png");
+    this.load.image("bullet", "assets/image/game-scene/components/bulletshot.png");
 
     this.load.spritesheet(
-      "goose",
-      "assets/image/game-scene/spritesheets/goose.png",
+      "player",
+      "assets/image/game-scene/spritesheets/player_sheet.png",
       {
-        frameWidth: 251, // * width of each frame devided by 7
-        frameHeight: 250,
+        frameWidth: 161, // * width of each frame devided by 7
+        frameHeight: 161,
       }
     );
 
-    this.load.image("logs", "assets/image/game-scene/components/logs.png");
-    this.load.image("enemy1", "assets/image/game-scene/components/logs.png");
-    this.load.image("enemy2", "assets/image/game-scene/components/logs.png");
-    this.load.image("enemy3", "assets/image/game-scene/components/logs.png");
+   
+    this.load.image("enemy1", "assets/image/game-scene/components/enemy1.png");
+    this.load.image("enemy2", "assets/image/game-scene/components/enemy2.png");
+    this.load.image("enemy3", "assets/image/game-scene/components/enemy3.png");
     this.load.image("platform", "assets/image/game-scene/platforms/ground.png");
   }
 
@@ -53,10 +56,14 @@ class GameScene extends Phaser.Scene {
     this.move = this.add.keyboard;
     this.shoot = this.input.keyboard.addKey("z");
     this.addAnimations();
+    this.boarder = this.physics.add.image(300,300,"boarder")
     this.bg = this.add
-      .tileSprite(0, 0, 1280, 720, "bg-pink") // x, y, width, height, key
+      .tileSprite(0, 0, 1280, 720, "backgound") // x, y, width, height, key
       .setOrigin(0, 0); // set origin to // ! top left
-    this.player = this.physics.add.sprite(200, 350, "goose");
+      this.boarder = this.physics.add.image(250,300,).setOffset(-10,-20).setSize(15, 720).setScale(2);
+      this.boarder.rotation = 4.72;
+    this.player = this.physics.add.sprite(200, 350, "player");
+   this.initUI() 
     this.player.setCollideWorldBounds(true);
     this.bullet = this.physics.add.group();
     this.bullet.enableBody = true;
@@ -69,12 +76,21 @@ class GameScene extends Phaser.Scene {
     this.physics.world.setBounds(0, 0, 1500, 750);
     this.physics.world.setBoundsCollision(true, true, true);
     // ? adding new platform by using tileSprite
-    this.platform = this.add
-      .tileSprite(0, 600, 1280, 100, "platform")
-      .setOrigin(0, 0);
-    this.groupObject = this.add.group();
+    // this.platform = this.add
+    //   .tileSprite(0, 650, 1280, 100, "platform")
+    //   .setOrigin(0, 0);
+
 
     enemies = this.physics.add.group();
+    this.physics.add.collider(
+      enemies,
+      this.boarder,() =>{
+        this.score = 0;
+        this.scene.start("Start");
+    },
+      null,
+      this
+    );
 
     this.physics.add.collider(
       this.bullet,
@@ -83,23 +99,25 @@ class GameScene extends Phaser.Scene {
       null,
       this
     );
+    
     this.player.setSize(200, 200).setOffset(0, 0.4);
   }
 
   update(time) {
     this.playermovement();
-    this.bg.tilePositionX += 1;
+    
 
     this.player.anims.play("player-walk", true);
     this.respawn();
-    enemies.setVelocityX(-200);
+    enemies.setVelocityX(-150);
     if (this.shoot.isDown) {
       if (this.time.now > shortime) {
-        this.bullet.create(this.player.x, this.player.y, "bullet");
+        this.bullet.create(this.player.x, this.player.y, "bullet").setScale(.2);
         this.bullet.setVelocityX(500);
         shortime = this.time.now + 400;
       }
     }
+    this.info.setText(`Score : ${this.score}`);
   }
   // ! move
   playermovement() {
@@ -114,11 +132,11 @@ class GameScene extends Phaser.Scene {
   addAnimations() {
     this.anims.create({
       key: "player-walk",
-      frames: this.anims.generateFrameNumbers("goose", {
+      frames: this.anims.generateFrameNumbers("player", {
         start: 0,
-        end: 6,
+        end: 1,
       }),
-      frameRate: 10,
+      frameRate: 2,
       repeat: -1,
     });
   }
@@ -155,11 +173,11 @@ class GameScene extends Phaser.Scene {
 
   scoreSystem(enemy) {
     if (this.getHitsNeeded(enemy) === 5) {
-      this.score += 5;
+      this.score += 10;
     } else if (this.getHitsNeeded(enemy) === 3) {
-      this.score += 3;
-    } else if (this.getHitsNeeded(enemy) === 2) {
       this.score += 5;
+    } else if (this.getHitsNeeded(enemy) === 2) {
+      this.score += 3;
     }
   }
 
@@ -211,6 +229,12 @@ class GameScene extends Phaser.Scene {
         this.spawntime = this.time.now + 1200;
       }
     }
+  }
+  initUI() {
+    //  Display the game stats
+    this.info = this.add
+      .text(10, 10, "", { font: "52px Arial", fill: "#ffffff" })
+      .setDepth(10);
   }
 }
 export default GameScene;
